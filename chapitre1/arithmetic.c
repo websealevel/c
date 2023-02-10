94afd540f03701c762cbaf186beb59264ed01a0f
websealevel <contact@websealevel.com>
2023-02-10 23:21:33 +0100

    introduit les constantes symboliques

--- chapitre1/arithmetic.c
+++ chapitre1/arithmetic.c
@@ -2,29 +2,25 @@
 
 /* affiche une table de conversion des degrés en Fahrenheit-Celsius*/
 
+#define LOWER 0   /*limite basse de la table*/
+#define UPPER 300 /*limite haute de la table*/
+#define STEP 20   /* taille du pas*/
+
 int main()
 {
     // Déclaration (placé avant toute instruction)
     float fahrenheit;
     float celsius;
-    int lower;
-    int upper;
-    int step;
 
-    // Assignement
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahrenheit = lower;
+    fahrenheit = LOWER;
 
     // Header, aligné avec le tableau.
-    printf("%12s\t%12s\n","Fahrenheit", "Celsius");
+    printf("%12s\t%12s\n", "Fahrenheit", "Celsius");
 
-    while (fahrenheit <= upper)
+    while (fahrenheit <= UPPER)
     {
-        celsius = (5.0/9.0) * (fahrenheit - 32);
+        celsius = (5.0 / 9.0) * (fahrenheit - 32);
         printf("%12.0f\t%12.2f\n", fahrenheit, celsius);
-        fahrenheit = fahrenheit + step;
+        fahrenheit = fahrenheit + STEP;
     }
 }
